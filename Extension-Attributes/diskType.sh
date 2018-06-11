d25c7dd5527f1686ed4843723676ba683c33d8bc
Anthony Moscaritola <amoscaritola@me.com>
2018-06-11 17:12:45 -0400

    commit message

---
+++ Extension-Attributes/diskType.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##########################################################################################################
+#   disktype.sh                                                                                          #
+#   Extension attribute that will list the types of drives in the Mac as well as if Fusion is set up.    #
+#   https://github.com/amoscaritola                                                                      #
+#   March 15, 2018                                                                                       #
+##########################################################################################################
+
+echo -n "<result>"
+diskutil info -all | grep "Solid State" | awk '{ print $3 }' | sort | uniq | sed 's/Yes/SSD/g' | sed 's/No/HDD/g'
+diskutil info -all | grep "Fusion Drive" | awk '{ print $3 }' | sort | uniq | sed 's/Yes/Fusion/g' | sed 's/No//g'
+echo "</result>"
