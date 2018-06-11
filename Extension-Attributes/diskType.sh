19f3711bb523b19daf4fe1aa073839d515c5c4af
Anthony Moscaritola <40155482+amoscaritola@users.noreply.github.com>
2018-06-11 17:17:49 -0400

    Update diskType.sh

--- Extension-Attributes/diskType.sh
+++ Extension-Attributes/diskType.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##########################################################################################################
-#   disktype.sh                                                                                          #
+#   diskType.sh                                                                                          #
 #   Extension attribute that will list the types of drives in the Mac as well as if Fusion is set up.    #
 #   https://github.com/amoscaritola                                                                      #
 #   March 15, 2018                                                                                       #
