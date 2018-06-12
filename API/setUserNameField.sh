e90ef0e95cee33c603c79e5caac3b1d9484a1e72
Anthony Moscaritola <amoscaritola@me.com>
2018-06-11 21:40:51 -0400

    Added setUserNameField.sh

---
+++ API/setUserNameField.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+######################################################################################################
+#   setUserNameField.sh
+#   Uses the current signed in username and assigns it to the username field of that mac in the JSS
+#   https://github.com/amoscaritola
+#   03.14.2018
+#######################################################################################################
+
+server="https://your.jss.server:8443" # JSS URL
+username="$4" # JSS username with API privileges - Parameter 4 in JSS
+password="$5" # Password for the JSS account - Parameter 5 in JSS
+
+currentUser=$(stat -f%Su /dev/console)
+
+macSerial=$(system_profiler SPHardwareDataType | grep Serial | awk '{ print $4 }')
+
+curl -k -s -H "Content-Type: application/xml" -u ${username}:${password} ${server}/JSSResource/computers/serialnumber/$macSerial -d "<computer><location><username>$currentUser</username></location></computer>" -X PUT
+
+exit 0
